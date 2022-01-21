432c8763a43ea70cf8159823f9a9425e4fb18153
Vishal Mittal <mittalvishal99@gmail.com>
2022-01-21 16:27:37 +0530

    21

--- com.mittal.Sliding.Window/src/com/mittal/Sliding/Window/MaximumSubArraySum.java
+++ com.mittal.Sliding.Window/src/com/mittal/Sliding/Window/MaximumSubArraySum.java
@@ -1,10 +1,48 @@
 package com.mittal.Sliding.Window;
 
-public class MaximumSubArraySum {
+/*
+ * Given an array of integers Arr of size N and a number K.
+ *  Return the maximum sum of a subarray of size K.
+ * Input:
+N = 4, K = 2
+Arr = [100, 200, 300, 400]
+Output:
+700 
+ */
 
+public class MaximumSubArraySum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int N=4;
+		int arr[]= {199,200,300,400};
+		int K=2;
+        int sum=0;
+        int maxsum=0;
+        //array ko n-k+1 length tak chalana hai
+        for(int i=0;i<N-K+1;i++)
+        {
+            sum=0;
+            // for each value of i uska window size tak karna hai i+k
+            for(int j=i;j<i+K;j++)
+            {
+                sum=sum+arr[j];
+            }
+            if(sum>maxsum)
+            {
+                maxsum=sum;
+            }
+        }
+        System.out.println(maxsum);
 
 	}
+	/*
+	 * We can see
+	 * 1) We are doing duplicate work sum ko 100+200+300
+	 *  fir sum ko 200+300+400
+	 *  why
+	 *  and see given data in question window hai
+	 *  to try ki sliding window lagay
+	 * 
+	 */
 
 }
--- com.mittal.Sliding.Window/src/com/mittal/Sliding/Window/MaximumSubArraySumB.java
+++ com.mittal.Sliding.Window/src/com/mittal/Sliding/Window/MaximumSubArraySumB.java
@@ -1,10 +1,41 @@
 package com.mittal.Sliding.Window;
-
+/*
+ * this is better approach
+ * why we thought of this??
+ * first see naive approach
+ * IN THIS QUESTION SEE HOW WE ATTEMPT WINDOW SLIDING
+ */
 public class MaximumSubArraySumB {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	public static void main(String[] args) 
+	{
+		int N=4;
+		int arr[]= {199,200,300,400};
+		int K=2;
+        int sum=0;
+        int maxsum=0;
+       
+        int i=0;
+        int j=0;
+        // i is starting of window and j is ending
+        while(j<N)// j<N we have to work till ending of window end of array ko hit nhi krti 
+        {
+            sum=sum+arr[j]; //MAIN TASK kya karna hai sum
+            if((j-i+1)<K)
+            {
+                j++;		       //FIRST---create karo window ko fir iss loop mei kabhi nahi aana
+            }
+            else if((j-i+1)==K) //SECOND--agar window ho gyi fir operations karo
+                {
+                    if(sum>maxsum)
+                    {
+                    maxsum=sum;   
+                    }
+                    sum=sum-arr[i];
+                    i++;
+                    j++;
+                }
+        }
+        System.out.println(maxsum);
+		}
 }
