f7b4435f4947ff4c722e783aeff0a211416f6a8c
Vishal Mittal <mittalvishal99@gmail.com>
2022-01-23 21:14:40 +0530

    ok

--- com.mittal.Sliding.Window/src/com/mittal/Sliding/Window/MaxValueInSizekB.java
+++ com.mittal.Sliding.Window/src/com/mittal/Sliding/Window/MaxValueInSizekB.java
@@ -1,5 +1,54 @@
 package com.mittal.Sliding.Window;
 
 public class MaxValueInSizekB {
+	 static ArrayList<Integer> max_of_subarrays(int arr[], int n, int k) 
+	    {
+	        ArrayList<Integer>arr2=new ArrayList<>();
+	        Deque<Integer>q=new ArrayDeque<>();
+	        
+	        int j=0;
+	        int i=0;
+	        while(j<n)
+	        {
+	            if(q.isEmpty()==true)
+	            {
+	                q.offer(arr[j]);
+	            }
+	            else if(q.getLast()<arr[j])
+	            {
+	                while(q.isEmpty()==false && q.getLast()<arr[j])
+	                {
+	                q.removeLast();
+	                }
+	                q.offer(arr[j]);
+	            }
+	            else{
+	                q.offer(arr[j]);
+
+	            }
+	            
+	            if((j-i+1)<k){
+	                j++;    
+	            }
+	            else
+	            {
+	                if((j-i+1)==k)
+	                {
+	                    arr2.add(q.peek());
+	                }
+	                if(arr[i]==q.peek())
+	                {
+	                    q.poll();
+	                    i++;
+	                    j++;
+	                }else{
+	                    i++;
+	                    j++;
+	                }
+	                
+	            }
+	        }
+	        return arr2;
+	   
 
 }
